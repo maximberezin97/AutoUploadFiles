8d3db3d5626993394180735337ac1d3af6cd5509
Maxim Berezin <maximberezin97@gmail.com>
2016-07-30 19:31:16 -0400

    Add JavaDoc-style comments to Controller class.

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -11,7 +11,13 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.List;
 
-//Maxim Berezin - 16 July 2016
+/**
+ * The main controller class of the JavaFX program.
+ * Contains both the main(String[]) function to be run by JVM,
+ * and the {@link Application}.start() function to be run by JavaFX.
+ * Also handles the creation and management of JavaFX Application Thread
+ * and the Task thread.
+ */
 public class Controller extends Application {
     public static final String name = "AutoUploadFiles";
     public static final String version = "3.6";
@@ -24,11 +30,32 @@ public class Controller extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    /**
+     * Overrides the {@link Application}.start() function
+     * that starts the JavaFX program.
+     * @param primaryStage  {@link Stage} of the initial window.
+     * @throws Exception    Necessary for override.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         MainWindow mainWindow = new MainWindow(this, primaryStage);
         mainWindow.run();
     }
+
+    /**
+     * Starts the {@link javafx.concurrent.Task} {@link Thread} that executes the FTP file upload.
+     * @param hostname      hostname for the FTP server
+     * @param port          port for the FTP server
+     * @param username      username for the FTP server login
+     * @param password      password for the FTP server login
+     * @param uploadPath    path on FTP server to upload to
+     * @param reuseSsl      reuse or create new SSL context for upload connection
+     * @param passiveMode   passive or active mode for the FTP server
+     * @param implicit      implicit or explicit connection for the FTP server
+     * @param printErrors   redirect error {@link PrintStream} for errors, used for debugging error messages
+     * @param files         {@link File}(s) to upload to the FTP server
+     */
     public void startUploaderTask(String hostname, int port, String username, String password, String uploadPath,
                                   boolean reuseSsl, boolean passiveMode, boolean implicit, boolean printErrors, List<File> files) {
         uploaderTask = new UploaderTask(this, hostname, port, username, password, uploadPath,
@@ -43,6 +70,13 @@ public class Controller extends Application {
         uploaderThread.setDaemon(true);
         uploaderThread.start();
     }
+
+    /**
+     * Attempts to cancel the FTP file upload.
+     * First attempts to send FTP command "ABOR" to server.
+     * Prints error stack trace if {@link IOException} is thrown/caught.
+     * Then sets the {@link javafx.concurrent.Task} state to "CANCELLED"
+     */
     public void cancelUploaderTask() {
         try {
             uploaderTask.cancelFtpUpload();
@@ -53,12 +87,23 @@ public class Controller extends Application {
         }
     }
 
+    /**
+     * Redirects {@link System}.out to {@link PrintStream} {@param prs}.
+     * @param prs           {@link PrintStream} to redirect {@link System}.out to
+     * @param transferErrors    does or does not redirect {@link System}.err to {@param prs}, used for debugging
+     */
     public void redirectOutput(PrintStream prs, boolean transferErrors) {
         System.setOut(prs);
         if(transferErrors) {
             System.setErr(prs);
         }
     }
+
+    /**
+     * Creates an {@link Alert} showing the {@param message} and the icon {@alertType}.
+     * @param message   The text of the message shown in the {@link Alert}
+     * @param alertType The icon shown in the {@link Alert} to designate success/failure/error
+     */
     public void showAlert(String message, Alert.AlertType alertType) {
         Alert alert = new Alert(alertType);
         alert.setTitle(dialogTitle);
@@ -69,15 +114,39 @@ public class Controller extends Application {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    /**
+     * Returns the current instance of the {@link UploaderTask} in use.
+     * @return  Current instance of {@link UploaderTask}.
+     */
     public UploaderTask getUploaderTask() {
         return uploaderTask;
     }
+
+    /**
+     * Returns the title of the dialog.
+     * Used for the title of {@link Stage}s in case of a name change.
+     * @return {@link String} of the dialog title.
+     */
     public String getDialogTitle() {
         return dialogTitle;
     }
+
+    /**
+     * Returns the icon of the program.
+     * Icon is an {@link Image} object containing an image file.
+     * Used as the icon of the program and in windows.
+     * @return  {@link Image} of the icon.
+     */
     public Image getIcon() {
         return icon;
     }
+
+    /**
+     * Returns the width in characters of the text fields
+     * in the {@link UploaderWindow} in case of a width adjustment.
+     * @return  int of the width of the text fields in character widths.
+     */
     public static int getTextFieldWidth() {
         return textFieldWidth;
     }
